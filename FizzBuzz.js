b327c3d3b49509efa65fb2c4286f038476bde1ca
Rakib-webdev-code <tamimislam100100@gmail.com>
2026-07-26 15:36:24 +0600

    fizz-buzz add

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,4 +1,33 @@
 // FizzBuzz ber kora kono vagses 0 hole
 // number  % 3 === 0 fizz
-// number % 5 === 0 Buzz 
+// number % 5 === 0 Buzz
 
+for (i = 1; i <= 10; i++) {
+  console.log("i =>", i);
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log("FizzBuzz");
+  } else if (i % 3 === 0) {
+    console.log("Fizz");
+  } else if (i % 3 === 0) {
+    console.log("Buzz");
+  } else {
+    console.log(i);
+  }
+}
+
+function FizzBuzz(number) {
+  for (i = 1; i <= number; i++) {
+    console.log("i =>", i);
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log("FizzBuzz");
+    } else if (i % 3 === 0) {
+      console.log("Fizz");
+    } else if (i % 3 === 0) {
+      console.log("Buzz");
+    } else {
+      console.log(i);
+    }
+  }
+}
+
+FizzBuzz(20);
